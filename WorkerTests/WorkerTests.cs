d18b6999aab2d43966426f44f45e1fcc8f6987e0
Andreas Olsson <olssonandreas@hotmail.com>
2017-06-14 22:47:04 +0200

    Added test project

--- WorkerTests/WorkerTests.cs
+++ WorkerTests/WorkerTests.cs
@@ -14,15 +14,22 @@ namespace WorkerTests
     public class WorkerTests
     {
         [TestMethod]
-        public async void WorkerCanStart()
+        public void WorkerCanStart()
         {
             var fileName = ConfigurationManager.AppSettings["FileName"];
             var arguments = ConfigurationManager.AppSettings["Arguments"];
             var intervalSeconds = int.Parse(ConfigurationManager.AppSettings["IntervalSeconds"]);
             var waitForExit = bool.Parse(ConfigurationManager.AppSettings["WaitForExit"]);
+            int waitTimeoutSeconds = 0;
+            int waitTimeout = 0;
+            var parseSuccess = int.TryParse(ConfigurationManager.AppSettings["WaitTimeoutSeconds"], out waitTimeoutSeconds);
+            if (parseSuccess)
+            {
+                waitTimeout = waitTimeoutSeconds * 1000;
+            }
 
             var subscriber = Substitute.For<IWorkerExecutedSubscriber>();
-            var worker = new Worker.Worker(fileName, arguments, intervalSeconds, waitForExit);
+            var worker = new Worker.Worker(fileName, arguments, intervalSeconds, waitForExit, waitTimeout, useShellExecute: true);
             worker.WorkerExecutedEvent += subscriber.ExecutedCallback;
             worker.StartWorker();
             subscriber.Received().ExecutedCallback(Arg.Any<object>(), Arg.Any<ElapsedEventArgs>());
